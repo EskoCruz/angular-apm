ae09d0b8bfe38dc9cf7d84e16bd3a2d5c861f15b
Esko Cruzeta <eskocruz@users.noreply.github.com>
2019-04-28 07:55:00 -0700

    populate list with product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import {ProductService} from './products/product.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
